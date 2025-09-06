536141308f6841c21532694e709ea90da7823aed
Catherine-Awuletey <catherineawuletey@gmail.com>
2025-09-06 20:01:26 +0000

    Fixed Image Path

--- src/components/construction.tsx
+++ src/components/construction.tsx
@@ -1,7 +1,9 @@
+import ConstructionImage from "../assets/construction.png";
+
 const construction = () => {
     return ( 
         <div className="flex flex-col items-center justify-center">
-            <img src="/src/assets/construction.png" alt="Under Construction" className="w-100 h-100 mb-8"/>
+            <img src={ConstructionImage} alt="Under Construction" className="w-100 h-100 mb-8"/>
             <h1 className="text-4xl font-bold mb-4 text-black">Page Under Construction</h1>
             <p className="text-lg text-gray-700">We're working hard to bring you this page. Stay tuned!</p>
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.jpg";
 
 
 const Navbar = () => {
   return (
       <nav className="text-black flex justify-between py-2 px-5 mx-3 my-5 items-center border border-gray-400 rounded-full">
         {/* <p className="font-bold text-xl">Cathy's</p> */}
-        <img src="/src/assets/Logo.jpg" alt="Logo" className="h-10 w-10 rounded-full"/>
+        <img src={Logo} alt="Logo" className="h-10 w-10 rounded-full"/>
         <div className="flex gap-5">
           <Link to="/">Home</Link>
           <Link to="/shop">Shop</Link>
