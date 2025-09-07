217db4ae915c8d10ddaaf6e8389ee871770a3e01
Catherine-Awuletey <catherineawuletey@gmail.com>
2025-09-07 17:35:38 +0000

    Prcoduct Category Title

--- src/pages/ProductCategory.jsx
+++ src/pages/ProductCategory.jsx
@@ -11,8 +11,8 @@ const ProductCategory = () => {
   );
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 capitalize">{categoryName} Products</h1>
+    <div className="container mx-auto lg:mx-0 p-8">
+      <h1 className="text-2xl lg:text-3xl font-bold text-black mb-8 capitalize">{categoryName} Products</h1>
 
       {filteredProducts.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
